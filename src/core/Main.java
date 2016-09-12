a785a637e2987ec14993ea6f68a465543aafd83b
DodgerAkame <remy.simon@uha.fr>
2016-09-12 14:59:49 +0200

    Commit initial

---
+++ src/core/Main.java
@@ -0,0 +1,24 @@
+package core;
+
+import java.awt.image.RenderedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class Main {
+
+	/*public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			File file = new File(".\\src\\rsc\\Roster.txt");
+			Randomizer r = new Randomizer(file);
+			CharSmash character = r.drawChar();
+                        System.out.println("FNEZJFZO");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}*/
+
+}
